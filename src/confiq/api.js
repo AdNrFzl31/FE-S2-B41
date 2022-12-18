b5d39291dae4cc77afb48a81a85936164b4f8326
Ade Nur Faizal <adenufaizal31@gmail.com>
2022-12-18 21:53:47 +0700

    deploy

--- src/confiq/api.js
+++ src/confiq/api.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 export const API = axios.create({
-  baseURL: "http://localhost:5000/api/v1/",
+  baseURL: "http://be-s2-b41-production.up.railway.app/api/v1/",
 })
 
 export const setAuthToken = (token) => {
